905bcaa837b130e9ac6e5eabe25a76a8fbfe47bc
hamza5645 <hamzaosama5645@gmail.com>
2024-04-08 17:07:48 +0200

    Completed the profile view

--- Instagram/Core/TabBar/MainTabView.swift
+++ Instagram/Core/TabBar/MainTabView.swift
@@ -30,7 +30,7 @@ struct MainTabView: View {
                     Image(systemName: "heart")
                 }
             
-            Text("Profile")
+            ProfileView()
                 .tabItem {
                     Image(systemName: "person")
                 }
