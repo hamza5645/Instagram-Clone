3482b2f913aeb75e56e35e1bb8a4b90e54bc217a
hamza5645 <hamzaosama5645@gmail.com>
2024-04-08 15:06:02 +0200

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Hamza Osama on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
