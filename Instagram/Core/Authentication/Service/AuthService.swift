7cfe52c50a3a28b00306462bd10853972fee4dcf
hamza5645 <hamzaosama5645@outlook.com>
2024-04-16 22:26:02 +0200

    Connected creating users with the firebase fireStorge

--- Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -32,6 +34,9 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            print("DEBUG: Did create user..")
+            await self.uploadUserData(uid: result.user.uid, username: username, email: email)
+            print("DEBUG: Did upload user data..")
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
@@ -44,5 +49,11 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
