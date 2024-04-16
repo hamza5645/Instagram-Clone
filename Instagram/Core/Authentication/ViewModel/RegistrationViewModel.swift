93240dbd9c93e0b5f7986e17688cf9678d44d61a
hamza5645 <hamzaosama5645@outlook.com>
2024-04-16 22:54:41 +0200

    Made the app forget the login info after log out

--- Instagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ Instagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -14,5 +14,9 @@ class RegistrationViewModel: ObservableObject {
     
     func createUser() async throws {
         try await AuthService.shared.createUser(email: email, password: password, username: username)
+        
+        username = ""
+        email = ""
+        password = ""
     }
 }
