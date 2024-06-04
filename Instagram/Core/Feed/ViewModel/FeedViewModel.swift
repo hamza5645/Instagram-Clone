b3429d446ac9491011e105fa8096146f1ded55e0
hamza5645 <hamzaosama5645@outlook.com>
2024-06-05 00:21:17 +0300

    Fetch feed posts completed

--- Instagram/Core/Feed/ViewModel/FeedViewModel.swift
+++ Instagram/Core/Feed/ViewModel/FeedViewModel.swift
@@ -20,9 +20,11 @@ class FeedViewModel: ObservableObject {
         let snapshot = try await Firestore.firestore().collection("posts").getDocuments()
         self.posts = try snapshot.documents.compactMap({ try $0.data(as: Post.self) })
         
-        for post in posts {
+        for i in 0 ..< posts.count {
+            let post = posts[i]
             let ownerUid = post.ownerUid
             let postUser = try await UserService.fetchUser(withUid: ownerUid)
+            posts[i].user = postUser
         }
     }
 }
